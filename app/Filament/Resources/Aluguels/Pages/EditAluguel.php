071868307036f59dfc5d3dad8c34a11d929c7c0e
gui120599 <gui120599@gmail.com>
2025-12-09 10:30:38 -0300

    Retirado o botão de excluir do aluguel

--- app/Filament/Resources/Aluguels/Pages/EditAluguel.php
+++ app/Filament/Resources/Aluguels/Pages/EditAluguel.php
@@ -13,7 +13,7 @@ class EditAluguel extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            //DeleteAction::make(),
         ];
     }
 
